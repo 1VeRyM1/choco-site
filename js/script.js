69e74fa7ae6d721a1a2d27dc5afb8d811057b838
VeRyM <verym2004@gmail.com>
2025-02-12 12:38:36 +1100

    Add page top, page mem and style top-block, style mem-block

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// function loadComponent(url, elementId) {
+//     fetch(url)
+//         .then(response => response.text())
+//         .then(data => {
+//             document.getElementById(elementId).innerHTML = data;
+//         });
+// }
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     loadComponent("header.html", "header-container");
+//     loadComponent("footer.html", "footer-container");
+// });
